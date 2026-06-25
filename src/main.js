75e035bef3223818baccdcade1d8cf43bfedc4a4
Franco Genoff <francogenoff@gmail.com>
2026-06-25 17:22:12 +0000

    Diseño terminado de la pagina Inicio, con boton seleccionado en la barra de navegacion

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import "./style.css";
 import { cargarInicio } from "./inicio";
 
-const contenedorPrincipal = document.querySelector("#contenedor");
 const paginaInicio = cargarInicio();
 
-contenedorPrincipal.appendChild(paginaInicio);
+const contenedorPrincipal = document.querySelector("#contenedor");
+const botonInicio = document.querySelector(".boton-inicio");
+const botonMenu = document.querySelector(".boton-menu");
+const botonContacto = document.querySelector(".boton-contacto");
+
+function actualizarVentanaActiva() {
+    botonInicio.classList.remove("activo")
+    botonMenu.classList.remove("activo")
+    botonContacto.classList.remove("activo")
+
+    botonActivo.classList.add("activo");
+}
+
+function inicializarPagina() {
+    contenedorPrincipal.appendChild(paginaInicio);
+    botonInicio.classList.add("activo");
+}
+
+// Eventos para los clics en el navegador
+botonInicio.addEventListener("click", () => {
+    contenedorPrincipal.textContent = "";
+    contenedorPrincipal.appendChild(cargarInicio());
+    actualizarVentanaActiva(botonInicio);
+});
+
+inicializarPagina();
+
+
